2d082fae82bad785698f3fd3f3a01114a5eff863
ZhannaVK <80942049+ZhannaVK@users.noreply.github.com>
2021-11-08 14:39:26 +0500

    hw5
    hw5

---
+++ src/components/chat/Chat.js
@@ -0,0 +1,17 @@
+import { ListItem, ListItemText } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
+
+//вывод одного элемента списка чатов
+export const OneChat = ({ name, id }) => {
+    return (
+        <ListItem component={Link} to={`/chats/${id}`}>
+            <ListItemText>{name} - {id}</ListItemText>
+        </ListItem>
+    );
+};
+
+OneChat.propTypes = {
+    id: propTypes.string,
+    name: propTypes.string
+}
